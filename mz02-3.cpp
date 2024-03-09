a75bfee07a2f6b330a95ef446b8ba2dae66418b1
xyliganchik2007 <yarikoval2512@mail.ru>
2024-03-09 17:02:33 +0300

    mz02-3

--- mz02-3.cpp
+++ mz02-3.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 
-int main()
-{
-    using std::cout, std::cin, std::hex;
-    int c;
-    int prev_c = -1, count = 0;
-    while ((c = cin.get()) != EOF) {
-        if (c == prev_c) {
-            count++;
-        } else {
-            if ((char) prev_c == '#' || count > 4) {
-                cout << '#' << (char) prev_c << hex << count << '#';
-            } else {
-                for (int i = 0; i < count; ++i) {
-                    cout << (char) prev_c;
-                }
-            }
-            count = 1;
-        }
-        prev_c = c;
-    }
-    if ((char) prev_c == '#' || count > 4) {
+constexpr int MAX_COUNT = 4;
+
+void print(char prev_c, int count) {
+    using std::cout, std::hex;
+    if ((char) prev_c == '#' || count > MAX_COUNT) {
         cout << '#' << (char) prev_c << hex << count << '#';
     } else {
         for (int i = 0; i < count; ++i) {
             cout << (char) prev_c;
         }
     }
+}
+
+int main()
+{
+    using std::cin;
+    int c;
+    int prev_c = -1, count = 0;
+    while ((c = cin.get()) != EOF) {
+        if (c == prev_c) {
+            count++;
+        } else {
+            print((char) prev_c, count);
+            count = 1;
+        }
+        prev_c = c;
+    }
+    print((char) prev_c, count);
     return 0;
 }
