65c9929077664daaccbca153ffc16c3ad9b030ab
xyliganchik2007 <yarikoval2512@mail.ru>
2024-03-17 22:01:52 +0300

    mz04

--- mz04-3.cpp
+++ mz04-3.cpp
@@ -1,3 +1,19 @@
-//
-// Created by yarik on 11.03.2024.
-//
+#include <vector>
+#include <algorithm>
+
+void
+process(const std::vector<int> &v1, std::vector<int> &v2)
+{
+    std::vector<int> v1_copy(v1);
+    std::sort(v1_copy.begin(), v1_copy.end());
+    std::vector<int>::iterator new_end = std::unique(v1_copy.begin(), v1_copy.end());
+    v1_copy.erase(new_end, v1_copy.end());
+    int count = 0;
+    for (const int &x: v1_copy) {
+        if (x - count < 0 || x - count >= (int) v2.size()) {
+            continue;
+        }
+        v2.erase(v2.begin() + x - count);
+        count++;
+    }
+}
