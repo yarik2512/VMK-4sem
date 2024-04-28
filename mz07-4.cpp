b696b6dc6c6fa1f7f856c7abba254c654e86aa84
xyliganchik2007 <yarikoval2512@mail.ru>
2024-04-28 20:40:58 +0300

    mz07

--- mz07-4.cpp
+++ mz07-4.cpp
@@ -1,1 +1,20 @@
+#include <algorithm>
 
+namespace Game {
+template<typename T>
+struct Coord {
+    typedef T value_type;
+    value_type col, row;
+
+    Coord(value_type c = 0, value_type r = 0) : col(c), row(r) {}
+};
+
+template<typename T>
+T dist(const Coord<T> &sz, const Coord<T> &a, const Coord<T> &b) {
+    T aq = a.col - (a.row + (a.row & 1)) / 2;
+    T ar = a.row;
+    T bq = b.col - (b.row + (b.row & 1)) / 2;
+    T br = b.row;
+    return std::max({std::abs(aq - bq), std::abs(ar - br), std::abs(-aq - ar + bq + br)});
+}
+}
